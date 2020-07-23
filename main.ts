279bd6ccfaa8b340e0d4f878aa8a9b6fe49276ad
htamarono <68046286+htamarono@users.noreply.github.com>
2020-07-23 14:21:16 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,14 @@
-let pos_y = 2
 let direction_y = 1
+let pos_y = 0
 led.plot(0, pos_y)
 basic.forever(function () {
     led.unplot(0, pos_y)
-    pos_y = direction_y
+    pos_y += direction_y
+    led.plot(0, pos_y)
     if (pos_y >= 4) {
-        direction_y += 1
+        direction_y = -1
     } else if (pos_y <= 0) {
-        direction_y += -1
+        direction_y = 1
     }
+    basic.pause(1000)
 })
